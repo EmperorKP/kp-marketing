405da7bbe285ab71087e23a5857d90951a05169a
KRISHNA PRASATH <kprasath@kalvium.community>
2024-04-02 20:03:16 +0530

    debug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,16 @@
+
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const font = Poppins({ subsets: ["latin"],weight:'400' });
+const font = Poppins(
+  { subsets: ["latin"],
+    weight: '400'
+}
+  );
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body 
+      
+      suppressHydrationWarning={true}
+      className={font.className}>
+        <Analytics />
+        <SpeedInsights />
+        {children}
+        
+        </body>
     </html>
   );
 }
